55565b3148ecf4f957a284d81740f56444598783
KhoaEm <khoa.em30041997@gmai.com>
2019-07-10 01:50:37 +0700

    sửa học sinh hoàn thành

--- app/src/main/java/com/example/preschool/StudentActivity.java
+++ app/src/main/java/com/example/preschool/StudentActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,8 +20,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -31,7 +35,7 @@ public class StudentActivity extends AppCompatActivity {
     private ProgressDialog loadingBar;
     private Bundle bundle;
     private ImageView imageView;
-    private FloatingActionButton floatingActionButton;
+    private FloatingActionButton btnAdd,btnEdit,btnSave,btnCancel;
     private static final int Gallery_Pick = 1,GALLERY = 1;
     private Uri ImageUri;
     private StorageReference ImageRef;
@@ -42,26 +46,87 @@ public class StudentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student);
 
         imageView=findViewById(R.id.btn_select_image);
-        floatingActionButton=findViewById(R.id.floating_add_image);
+        btnAdd=findViewById(R.id.floating_add_image);
+        btnEdit=findViewById(R.id.floating_edit_image);
+        btnSave=findViewById(R.id.floating_save_image);
+        btnCancel=findViewById(R.id.floating_cancel_image);
+
         bundle=getIntent().getExtras();
         idClass=bundle.getString("ID_CLASS");
         idTeacher=bundle.getString("ID_TEACHER");
         loadingBar = new ProgressDialog(this);
         ImageRef = FirebaseStorage.getInstance().getReference().child(idClass);
-        ClassRef= FirebaseDatabase.getInstance().getReference().child("Class").child(idClass).child("Children");
+        ClassRef= FirebaseDatabase.getInstance().getReference().child("Class");
+        btnAdd.hide();
+        btnEdit.hide();
+        btnSave.hide();
+        btnCancel.hide();
+        ClassRef.child(idClass).child("Children").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String image=dataSnapshot.child("image").getValue().toString();
+                    Picasso.get().load(image).into(imageView);
+                    btnEdit.show();
+                    btnSave.hide();
+                    btnAdd.hide();
+                    imageView.setEnabled(false);
 
+                }
+                else
+                {
+                    btnAdd.show();
+                    btnEdit.hide();
+                    btnSave.hide();
+                    imageView.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 OpenGallery();
             }
         });
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ValidatePostInfo();
             }
         });
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+                btnAdd.hide();
+                btnEdit.hide();
+                btnSave.show();
+                btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ValidatePostInfo();
+                    }
+                });
+                btnCancel.show();
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        btnCancel.hide();
+                        btnSave.hide();
+                        btnEdit.show();
+                        imageView.setEnabled(false);
+                        recreate();
+                    }
+                });
+            }
+        });
+
     }
     private void ValidatePostInfo() {
         if (ImageUri == null) {
@@ -88,14 +153,17 @@ public class StudentActivity extends AppCompatActivity {
                                 public void onSuccess(Uri uri) {
                                     final String downloadUrl = uri.toString();
 
-                                    ClassRef.child("image").setValue(downloadUrl)
+                                    ClassRef.child(idClass).child("Children").child("image").setValue(downloadUrl)
                                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                                 @Override
                                                 public void onComplete(@NonNull Task<Void> task) {
                                                     if (task.isSuccessful()) {
                                                         loadingBar.dismiss();
                                                         Toast.makeText(StudentActivity.this, "Children Image stored to Firebase Database Successfully...", Toast.LENGTH_SHORT).show();
-
+                                                        imageView.setEnabled(false);
+                                                        btnEdit.show();
+                                                        btnAdd.hide();
+                                                        btnSave.hide();
                                                     } else {
                                                         String message = task.getException().getMessage();
                                                         Toast.makeText(StudentActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
